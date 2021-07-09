9b40bb0311ac03e8fb89a0172daf2295d219a963
gasperpb <you@example.com>
2021-07-09 14:47:10 -0300

    Organization of Readme

---
+++ into_lista.py
@@ -0,0 +1,25 @@
+"""  Exercícios
+Abaixo alguns exercícios para praticar o que foi ensinado. Você pode resolvê-los diretamente no
+interpretador ou usando a IDE Geany.
+1. Armazene os nomes de alguns de seus amigos em uma lista chamada nomes. Exiba o nome
+de cada pessoa acessando cada elemento da lista, um de cada vez.
+2. Digamos que você vá oferecer uma festa de aniversário. 
+1. Crie uma lista de convidados.
+2. Exiba uma mensagem convite para cada convidado.
+3. Alguns convidados não poderão comparecer, mas sugeriram outro nome para substituílos. Embora você não tenha gostado da ideia você a acatou. Modifique a lista para
+contemplar essa situação.
+4. Você conseguiu alugar um espaço maior para a festa e resolveu expandir a lista de
+convidados. Acrescente 3 novos convidados:
+1. Usando insert() para adicionar um novo convidado no início da lista.
+2. Usando insert() para adicionar um novo convidado no meio da lista.
+3. Usando append() para adicionar um novo convidado no fim da lista.
+5. Alguns convidados estão viajando e não poderão comparecer. Remova 3 convidados da
+lista:
+1. Usando pop() para remover um do fim.
+2. Usando remove() passando o nome de um dos convidados.
+3. Usando del para remover um do meio da lista.  """
+
+cursos = ['informática', 'meio ambiente', 'agroindústria']
+
+"""  1. Armazene os nomes de alguns de seus amigos em uma lista chamada nomes. Exiba o nome """
+nomes = ['achilles', 'Jardiele', 'Dilene', 'Wal', 'Ivonaldo', 'Paulinho']
